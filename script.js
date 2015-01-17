bf9fe0823eaf6cf9d455013fdc3f9583297e80b8
Russell <compsciwizkid@gmail.com>
2015-01-17 07:19:02 -0800

    step 4: adding state to Photo

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var Photo = React.createClass({
 
+  toggleLiked: function() {
+    this.setState({
+      liked: !this.state.liked
+    });
+  },
+
+  getInitialState: function() {
+    return {
+      liked: false
+    }
+  },
+
   render: function() {
+    var buttonClass = this.state.liked ? 'active' : '';
+
     return (
       <div className='photo'>
-        <img src={this.props.imageURL} />
-        <span>{this.props.caption}</span>
+        <img src={this.props.src} />
+
+        <div className='bar'>
+          <button onClick={this.toggleLiked} className={buttonClass}>
+            ♥
+          </button>
+          <span>{this.props.caption}</span>
+        </div>
       </div>
     )
   }
 });
 
-React.render(<Photo imageURL='http://tinyurl.com/lkevsb9' caption='New York!' />, document.body);
+React.render(<Photo src='http://tinyurl.com/lkevsb9' caption='New York!'/>, document.body);
